0c78fec59dde3275aabd0a7a5bbb2f77ad5c118d
leero <home>
2019-03-15 01:28:59 +0300

    airport map

--- app/src/main/java/com/example/aviato/fragment/PlaneFragment.kt
+++ app/src/main/java/com/example/aviato/fragment/PlaneFragment.kt
@@ -5,20 +5,29 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.PorterDuff
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 
-import com.example.aviato.R
 import com.example.aviato.item.AirportItem
+import com.example.aviato.item.RouteItem
 import com.google.android.gms.maps.*
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLngBounds
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
+import java.util.*
+import android.R.color
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Gap
+import com.google.android.gms.maps.model.Dash
+import com.google.android.gms.maps.model.PatternItem
+import java.util.Arrays.asList
+import com.google.android.gms.maps.model.PolylineOptions
+
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -32,6 +41,9 @@ private const val ARG_PARAM2 = "param2"
 class PlaneFragment : Fragment(), OnMapReadyCallback {
     private lateinit var firstAirport: AirportItem
     private lateinit var secondAirport: AirportItem
+    val STEP=0.01f
+    val PLANE_SPEED=50
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -52,6 +64,8 @@ class PlaneFragment : Fragment(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
+
+
         val builder = LatLngBounds.Builder()
         builder.include(firstAirport.latLng)
         builder.include(secondAirport.latLng)
@@ -61,8 +75,87 @@ class PlaneFragment : Fragment(), OnMapReadyCallback {
         val cameraUpdate = CameraUpdateFactory.newLatLngBounds(builder.build(), width, height, padding)
         googleMap?.moveCamera(cameraUpdate)
 
+
         setAirportNameMarker(googleMap, firstAirport)
         setAirportNameMarker(googleMap, secondAirport)
+
+        var route=getRoute()
+
+        drawLine(googleMap, route)
+
+    }
+
+    private fun drawLine(googleMap: GoogleMap?, route: ArrayList<RouteItem>) {
+        val line = java.util.ArrayList<LatLng>()
+        for (pathItem in route)
+            line.add(pathItem.latLng)
+
+        val polylineOptions = PolylineOptions()
+            .addAll(line)
+
+            .geodesic(false)
+            .width(10f)
+            .color(resources.getColor(R.color.colorPrimary))
+        val polyline = googleMap?.addPolyline(polylineOptions)
+        polyline?.pattern = Arrays.asList(Dot(), Gap(20f));
+
+        polyline?.jointType = JointType.DEFAULT
+
+    }
+
+    private fun getRoute(): ArrayList<RouteItem> {
+        val list = java.util.ArrayList<RouteItem>()
+
+        val startLatLang=firstAirport.latLng
+        val endLatLang=secondAirport.latLng
+
+        val length = Math.sqrt(Math.pow(startLatLang.latitude - endLatLang.latitude, 2.0
+            ) + Math.pow(startLatLang.longitude - endLatLang.longitude, 2.0))
+       // val angle = 0.0
+
+        val angle = getAngle(startLatLang, endLatLang)
+
+        val sin = Math.sin(angle)
+        val cos = Math.cos(angle)
+
+
+        list.add(RouteItem(startLatLang, 0.0))
+        run {
+            var i = 0.0
+            while (i < length) {
+                val x = if (startLatLang.longitude - endLatLang.longitude > 0) i * -1 else i
+
+                val latitude = (4 * Math.sin(x * 2.0 * Math.PI / length))
+
+                val newLat = (x * sin + latitude * cos + startLatLang.latitude)
+                val newLon = (x * cos + latitude * sin + startLatLang.longitude)
+
+                val latLng = LatLng(newLat, newLon)
+                list.add(RouteItem(latLng, 0.0))
+                i += STEP
+            }
+        }
+        // list.add(new PathItem(endLatLang,0));
+
+        for (i in 1 until list.size - 2) {
+            val pathItem = list[i]
+            pathItem.angle=(getAngleDegrees(list[i - 1].latLng, list[i + 1].latLng))
+        }
+
+        return list
+    }
+
+    private fun getAngle(startLatLang: LatLng, endLatLang: LatLng): Double {
+        return ((endLatLang.latitude - startLatLang.latitude) / (endLatLang.longitude - startLatLang.longitude))
+      //  return ((startLatLang.latitude - endLatLang.latitude) / (startLatLang.longitude - endLatLang.longitude))
+    }
+
+    private fun getAngleDegrees(startLatLang: LatLng, endLatLang: LatLng): Double {
+         if (startLatLang.longitude > endLatLang.longitude)
+             return     ((startLatLang.latitude - endLatLang.latitude) / (startLatLang.longitude - endLatLang.longitude) * (180 / Math.PI)) * -1 + 180
+        else
+        return ((startLatLang.latitude - endLatLang.latitude) / (startLatLang.longitude - endLatLang.longitude) * (180 / Math.PI)) * -1
+        // return (float) Math.tan(((startLatLang.latitude - endLatLang.latitude) / (startLatLang.longitude - endLatLang.longitude))/(Math.PI*180));
     }
 
     private fun setAirportNameMarker(googleMap: GoogleMap?, airport: AirportItem) {
@@ -90,6 +183,7 @@ class PlaneFragment : Fragment(), OnMapReadyCallback {
 
         val marker= googleMap?.addMarker(options)
     }
+
 
 
 }
---
+++ app/src/main/java/com/example/aviato/item/RouteItem.kt
@@ -0,0 +1,6 @@
+package com.example.aviato.item
+
+import com.google.android.gms.maps.model.LatLng
+
+class RouteItem (val latLng: LatLng, var angle:Double) {
+}
